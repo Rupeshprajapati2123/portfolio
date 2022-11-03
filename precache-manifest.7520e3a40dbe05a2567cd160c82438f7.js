a776b1b154afb5d1879ef6b61e0fffc237eb02a8
Rupesh Prajapati <rupesh_p@ch.iitr.ac.in>
2022-11-03 11:16:41 +0530

    Updates

---
+++ precache-manifest.7520e3a40dbe05a2567cd160c82438f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30336bb0740b466e6656860385a64695",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d66abf07b9ddd9da57e3",
+    "url": "/portfolio/static/css/2.f00fa05b.chunk.css"
+  },
+  {
+    "revision": "e155ac5729919cc7a1bc",
+    "url": "/portfolio/static/css/main.029945fe.chunk.css"
+  },
+  {
+    "revision": "d66abf07b9ddd9da57e3",
+    "url": "/portfolio/static/js/2.bba04dc7.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/portfolio/static/js/2.bba04dc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e155ac5729919cc7a1bc",
+    "url": "/portfolio/static/js/main.7cdf7f8e.chunk.js"
+  },
+  {
+    "revision": "a0b39d4ae00a5b0ac391",
+    "url": "/portfolio/static/js/runtime-main.9a002b9a.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/portfolio/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "fae3cbdfa2710f60ae85ac43f81d2125",
+    "url": "/portfolio/static/media/My-logo4.fae3cbdf.ico"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/portfolio/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/portfolio/static/media/laptop1.87561258.jpg"
+  }
+]);
